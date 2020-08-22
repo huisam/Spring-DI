d512728538c31bca730bb525f59a6b3e9e9cc855
huisam <huisam@naver.com>
2020-08-22 16:51:54 +0900

    feat: Bean 들을 읽어와서 initialize 하는 기능 구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,15 +1,15 @@
 package core.di.factory;
 
 import com.google.common.collect.Maps;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Constructor;
 import java.util.Map;
 import java.util.Set;
 
+@Slf4j
 public class BeanFactory {
-    private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
-
     private Set<Class<?>> preInstanticateBeans;
 
     private Map<Class<?>, Object> beans = Maps.newHashMap();
@@ -24,6 +24,32 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        for (Class<?> preInstanticateBean : preInstanticateBeans) {
+            instantiate(preInstanticateBean);
+        }
+    }
 
+    private Object instantiate(Class<?> preInstanticateBean) {
+        if (beans.containsKey(preInstanticateBean)) {
+            return beans.get(preInstanticateBean);
+        }
+
+        final Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(preInstanticateBean);
+        if (injectedConstructor == null) {
+            final Object instance = BeanUtils.instantiateClass(preInstanticateBean);
+            this.beans.put(preInstanticateBean, instance);
+            return instance;
+        }
+
+        final Class<?>[] parameterTypes = injectedConstructor.getParameterTypes();
+        final Object[] parametersInstances = new Object[parameterTypes.length];
+        for (int i = 0; i < parametersInstances.length; i++) {
+            final Class<?> concreteParameterTypes = BeanFactoryUtils.findConcreteClass(parameterTypes[i], preInstanticateBeans);
+            parametersInstances[i] = instantiate(concreteParameterTypes);
+        }
+
+        final Object instance = BeanUtils.instantiateClass(injectedConstructor, parametersInstances);
+        this.beans.put(preInstanticateBean, instance);
+        return instance;
     }
 }
